6afc67186cbca2e437e5baa64f82d3b236aa9a3e
VrodeYuliya <romanova.yulia5444@gmail.com>
2024-02-21 16:45:30 +0300

    add Button scaffold

--- app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
@@ -37,7 +37,7 @@ fun NavigationScreen() {
             NavigationButton(stringResource(id = R.string.Row), Screen.Row)
             NavigationButton(stringResource(id = R.string.Column), Screen.Column)
             NavigationButton(stringResource(id = R.string.Box), Screen.Box)
-            NavigationButton(stringResource(id = R.string.Surface), Screen.Surface)
+            NavigationButton(stringResource(id = R.string.Scaffold), Screen.Scaffold)
         }
     }
 }
