7cfefe968548279118c2dbfe6d614ae67e399314
VrodeYuliya <romanova.yulia5444@gmail.com>
2024-02-21 14:07:20 +0300

    add ButtonBox

--- app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
@@ -36,6 +36,7 @@ fun NavigationScreen() {
             NavigationButton(stringResource(id = R.string.button_alertDialog), Screen.AlertDialog)
             NavigationButton(stringResource(id = R.string.Row), Screen.Row)
             NavigationButton(stringResource(id = R.string.Column), Screen.Column)
+            NavigationButton(stringResource(id = R.string.Box), Screen.Box)
         }
     }
 }
